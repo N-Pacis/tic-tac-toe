0eac7cbdf4b1c9ee961522e67cdf128dc5448754
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-18 08:15:29 +0200

    [Feat] added main program messages

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,8 +1,29 @@
 package game;
 
-public class Main {
+import helpers.DatabaseConnection;
+import helpers.ErrorMessageLogger;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+    private static Integer choice;
+    private static Boolean loggedIn = false;
+    private static String userId;
+    private static ErrorMessageLogger error = new ErrorMessageLogger();
+
+    public static void main(String[] args) throws IOException, SQLException {
+        new DatabaseConnection();
+        do{
+            System.out.println("##################################################");
+            System.out.println(" ###### TIC TAC TOE GAME #####");
+            System.out.println("\t\t\t\t 1. Register to the system");
+            System.out.println("\t\t\t\t 2. Login to the system");
+            System.out.println("\t\t\t\t 0. Exit");
+
+            Scanner scan = new Scanner(System.in);
+            choice = scan.nextInt();
+        }while (choice != 0);
     }
 }
