449d20eafd328011e0f6ee85da8761def6d5b323
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-18 08:15:07 +0200

    Create DatabaseConnection.java

---
+++ src/helpers/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package helpers;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static String dbUrl = "jdbc:mysql://localhost/ticTacToe";
+    private static String dbUser = "root";
+    private static String dbPassword = "pacis123";
+    public static Connection myConnection;
+
+    public DatabaseConnection() {
+        try{
+            myConnection = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+            System.exit(-1);
+        }
+    }
+}
