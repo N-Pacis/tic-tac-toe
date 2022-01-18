26143bdf8627eb4ba96d296167bf9c670291b30e
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-18 08:14:16 +0200

    [Feat] added user service

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,4 +1,47 @@
 package services;
 
-public class UserService {
+import helpers.DatabaseConnection;
+
+import java.sql.*;
+
+public class UserService{
+    DatabaseConnection dbConn = new DatabaseConnection();
+    Connection conn = dbConn.myConnection;
+
+    public void registerUser(String userId,String email, String password) throws SQLException {
+        String sql = "INSERT INTO users(userId,email,password) VALUES(?,?,?,?)";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1,userId);
+        statement.setString(3,email);
+        statement.setString(4,password);
+        statement.executeUpdate();
+    }
+
+    public Boolean checkEmail(String email) throws SQLException {
+        String sql = "SELECT * FROM users WHERE email='"+email+"'";
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        int rowCount = 0;
+        while(result.next()){
+            rowCount++;
+        }
+        if(rowCount > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public String checkCredentials(String email,String password) throws SQLException {
+        String sql = "SELECT * FROM USERS WHERE email='"+email+"' and password='"+password+"'";
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        int rowCount = 0;
+        while(result.next()){
+            rowCount++;
+            if(rowCount == 1){
+                return result.getString("userId");
+            }
+        }
+        return "";
+    }
 }
