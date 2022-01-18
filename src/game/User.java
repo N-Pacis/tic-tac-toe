03f93ae74c231682355a7ebdf63cd272024ebaed
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-18 08:15:13 +0200

    Create User.java

---
+++ src/game/User.java
@@ -0,0 +1,77 @@
+package game;
+
+import helpers.ErrorMessageLogger;
+import helpers.SuccessMessageLogger;
+import helpers.UniqueRandomCharacters;
+import services.UserService;
+import validators.UserValidator;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class User {
+    UserValidator validator = new UserValidator();
+    UserService userService = new UserService();
+    private static Scanner scan = new Scanner(System.in);
+
+    private static String email;
+    private static String password;
+    private String userId;
+    private static ErrorMessageLogger error = new ErrorMessageLogger();
+    private static SuccessMessageLogger success = new SuccessMessageLogger();
+
+    public Boolean registerUser() throws IOException, SQLException {
+        UniqueRandomCharacters randomCharacters = new UniqueRandomCharacters();
+        String[] fields= {"email","password"};
+        System.out.println("### \t\tPLEASE FILL THE REQUIRED INFORMATION ###");
+        for(String field : fields){
+            generateInput(field,"register");
+        }
+        String userId = randomCharacters.random();
+        userService.registerUser(userId,email,password);
+        success.log("### REGISTERED SUCCESSFULLY ###");
+        setUserId(userId);
+        return true;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Boolean login() throws IOException, SQLException {
+        String[] fields = {"email","password"};
+        System.out.println("### \t\t ENTER YOUR ACCOUNT");
+        for(String field: fields){
+            generateInput(field,"login");
+        }
+        String checkCredentials = userService.checkCredentials(email,password);
+        if(!checkCredentials.isEmpty()){
+            setUserId(checkCredentials);
+            return true;
+        }
+        return false;
+    }
+
+    private void generateInput(String fieldName,String source) throws IOException, SQLException {
+        System.out.println("\t\t - ENTER YOUR "+fieldName.toUpperCase());
+        switch(fieldName){
+            case "email":
+                email = scan.nextLine().toLowerCase();
+                if(source.equals("register")){
+                    email = validator.checkEmail(email);
+                }
+                break;
+            case "password":
+                password = scan.nextLine();
+                password = validator.checkPassword(password);
+                break;
+            default:
+                error.log("Invalid field");
+        }
+    }
+}
