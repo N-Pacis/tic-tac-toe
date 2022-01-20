4ed6a6953c836c4b6fbc5104b5779388ee5bd7c8
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-20 14:03:48 +0200

    [Feat] added main choices

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,6 +4,7 @@ import helpers.DatabaseConnection;
 import helpers.ErrorMessageLogger;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -13,10 +14,10 @@ public class Main {
     private static String userId;
     private static ErrorMessageLogger error = new ErrorMessageLogger();
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, NoSuchAlgorithmException {
         new DatabaseConnection();
         do{
-            System.out.println("##################################################");
+            System.out.println("-------------------------------------------------------");
             System.out.println(" ###### TIC TAC TOE GAME #####");
             System.out.println("\t\t\t\t 1. Register to the system");
             System.out.println("\t\t\t\t 2. Login to the system");
@@ -24,6 +25,54 @@ public class Main {
 
             Scanner scan = new Scanner(System.in);
             choice = scan.nextInt();
-        }while (choice != 0);
+            User user = new User();
+
+            switch(choice){
+                case 1:
+                    if(user.registerUser()){
+                        userId = user.getUserId();
+                        loggedIn = true;
+                    }
+                    break;
+                case 2:
+                    if(!user.login()){
+                        error.log("### INVALID CREDENTIALS ###");
+                        user.login();
+                    }
+                    userId = user.getUserId();
+                    loggedIn = true;
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    error.log("!!! Invalid choice !!!");
+            }
+
+        }while (!loggedIn);
+        if(loggedIn){
+            do{
+                System.out.println("### CHOOSE A DIFFICULTY LEVEL BELOW ###");
+                System.out.println("\t\t\t\t 1. Beginner");
+                System.out.println("\t\t\t\t 2. Advanced");
+
+                System.out.println("\t\t\t\t 0. Exit");
+                System.out.println("PLEASE ENTER YOUR CHOICE:");
+
+                Scanner scan = new Scanner(System.in);
+                choice = scan.nextInt();
+
+                switch(choice){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 0:
+                        System.exit(0);
+                    default:
+                        error.log("!!! Invalid choice !!!");
+                }
+
+            }while(!choice.equals(0));
+        }
     }
 }
