b2425d16a7bdd5aca4ad0710208d2efcd2294d67
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-22 19:25:40 +0200

    [Feat] added drawing the game board

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,10 +2,23 @@ package game;
 
 public class Game {
     private static char[][] gameBoard = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    private void drawBoard(){
-
+    private static void drawBoard(){
+        for(int i = 0;i<gameBoard.length;i++){
+            for(int j = 0;j<gameBoard[i].length;j++){
+                if(j!= gameBoard.length -1 ){
+                    System.out.print(gameBoard[i][j] + " | ");
+                }
+                else{
+                    System.out.print(gameBoard[i][j]);
+                }
+            }
+            System.out.println();
+            if(i!= gameBoard.length -1){
+                System.out.println("--+--+--");
+            }
+        }
     }
     public void play(){
-
+        drawBoard();
     }
 }
